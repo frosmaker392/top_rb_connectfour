842489cfb1cccfdc2dc442be6c04ccbbbefed21c
zhafir <zzzhafir75@gmail.com>
2020-08-06 14:35:29 +0800

    Add more test for #insert

--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -42,5 +42,33 @@ describe ConnectFour do
                               [nil, nil, nil, nil, nil, nil], 
                               [nil, nil, nil, nil, nil, nil]])
     end
+
+    it "does not exceed the length of the array" do
+      c4 = ConnectFour.new
+      8.times { c4.insert(1, 3) }
+      expect(c4.grid).to eql([[nil, nil, nil, nil, nil, nil], 
+                              [nil, nil, nil, nil, nil, nil],
+                              [nil, nil, nil, nil, nil, nil],
+                              [1, 1, 1, 1, 1, 1], 
+                              [nil, nil, nil, nil, nil, nil], 
+                              [nil, nil, nil, nil, nil, nil], 
+                              [nil, nil, nil, nil, nil, nil]])
+    end
+
+    it "inserts different numbers" do
+      c4 = ConnectFour.new
+      4.times { c4.insert(1, 6); c4.insert(2, 6) }
+      c4.insert(1, 5)
+      c4.insert(2, 4)
+      c4.insert(1, 4)
+      c4.insert(2, 1)
+      expect(c4.grid).to eql([[nil, nil, nil, nil, nil, nil], 
+                              [2, nil, nil, nil, nil, nil],
+                              [nil, nil, nil, nil, nil, nil],
+                              [nil, nil, nil, nil, nil, nil], 
+                              [2, 1, nil, nil, nil, nil], 
+                              [1, nil, nil, nil, nil, nil], 
+                              [1, 2, 1, 2, 1, 2]])
+    end
   end
 end
