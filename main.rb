d66d1c27444c54b7b3f6ac5c21edb987cf51d80e
zhafir <zzzhafir75@gmail.com>
2020-08-08 19:36:37 +0800

    Add custom characters for P1 and P2 chips, add more messages

--- main.rb
+++ main.rb
@@ -3,6 +3,9 @@ require_relative 'lib/connect_four'
 
 connect_four = ConnectFour.new
 
+P1_CHAR = '●'
+P2_CHAR = '◌'
+
 def validate_column_num(input, valid_columns)
   raise "Input is not a number!" unless input =~ /^-?[0-9]+$/
   raise "Out of range!" unless (1..7).include?(input.to_i)
@@ -20,17 +23,18 @@ puts "    ----    Connect Four    ----    \n"\
      "\> The goal is to be the first one to get 4 of their chips in a row \n"\
      "  (diagonals count)\n\n"
 
+puts "Player 1 : #{P1_CHAR}, Player 2 : #{P2_CHAR}"
+
 # Start with random player
 current_player = [1, 2].sample
 result = -1
 
 # Game loop which ends when evalute does not return -1
 while result == -1
-  print "\n"
-  print connect_four.show_grid('a', 'b', true)
+  print connect_four.show_grid(P1_CHAR, P2_CHAR, true)
 
   # Prompt for player input
-  print "Player #{current_player}'s turn : "
+  print "\nPlayer #{current_player}'s turn : "
   col = Input.get_valid_input(lambda { |x| validate_column_num(x, connect_four.valid_columns) })
 
   connect_four.insert(current_player, col - 1)
@@ -38,6 +42,12 @@ while result == -1
   # Change player after each loop
   current_player = (current_player == 1 ? 2 : 1)
   result = connect_four.evaluate
+
+  print "\n"
 end
 
-puts result
+print connect_four.show_grid(P1_CHAR, P2_CHAR, true)
+print "\n"
+
+if result == 0 then puts "Draw! No one wins!"
+else puts "Player #{result} wins!" end
